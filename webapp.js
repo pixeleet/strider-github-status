565f1d30a649fce66e1a09f45c1f9d495520fcec
David Lonyai <david.lonyai@gmail.com>
2015-08-20 16:04:04 +0200

    debug mode

--- webapp.js
+++ webapp.js
@@ -9,18 +9,22 @@ function jobStatus(job) {
 }
 
 // TODO: give information here as to why id errored/failed?
-function jobDescription(job) {
+function jobDescription(job, projectName) {
 
     var description;
-    var runnerLogPath = path.resolve('/home/strider/.strider/data/pixeleet-sweebr-' + job._id, 'unit-test-runner.log');
-    var runnerLog = fs.existsSync(runnerLogPath) ? fs.readFileSync(runnerLogPath) : '';
-
-    if (job.errored) description = 'Strider tests errored'
-    description = 'Strider tests ' + (job.test_exitcode === 0 ? 'succeeded' : 'failed') + '\n' + runnerLog;
+    var slug = projectName.replace('/', '-');
+    var runnerLogPath = path.resolve(['/home/strider/.strider/data', [slug, job._id].join('-'), 'unit-test-runner.log'].join('/'));
+    var runnerLog = fs.existsSync(runnerLogPath) ? fs.readFileSync(runnerLogPath).toString() : '';
 
     debug('Job Data', JSON.Stringify(job, false, 4));
-    debug('LogPath', runnerLogPath);
+    debug('LogPath', slug, runnerLogPath);
     debug('Description', description);
+
+    if (job.errored) {
+        description = 'Strider tests errored'
+    } else {
+        description = 'Strider tests ' + (job.test_exitcode === 0 ? 'succeeded' : 'failed') + '\n' + runnerLog;
+    }
 
     return description;
 }
@@ -36,7 +40,7 @@ module.exports = {
 
         io.on('plugin.github-status.done', function(jobId, projectName, token, data) {
             var onDoneAndSaved = function(job) {
-                debug('onDoneAndSaved [job]', job);
+                debug('onDoneAndSaved [job]', Object.keys(job));
                 if (job._id.toString() !== jobId.toString()) return
                 debug('plugin done', jobId, projectName, token, data)
 
@@ -44,7 +48,7 @@ module.exports = {
                 debug('job data', job);
                 var url = context.config.server_name + '/' + projectName + '/job/' + jobId,
                     status = jobStatus(job),
-                    description = jobDescription(job)
+                    description = jobDescription(job, projectName)
                 setStatus(token, url, data, status, description)
             }
             io.on('job.doneAndSaved', onDoneAndSaved)
