59166498e626bb30117ac7cad5b4fb65a8a856a4
David Lonyai <david.lonyai@gmail.com>
2015-08-21 12:09:39 +0200

    add comment on pr

--- webapp.js
+++ webapp.js
@@ -1,5 +1,6 @@
-var setStatus = require('./lib/handler')
-  , debug = require('debug')('strider-github-status')
+var setStatus = require('./lib/handler').setStatus
+  , createComment = require('./lib/handler').createComment
+  , debug = require('debug')('strider-github-status');
 
 function jobStatus(job) {
   if (job.errored) return 'error'
@@ -10,6 +11,26 @@ function jobStatus(job) {
 function jobDescription(job) {
   if (job.errored) return 'Strider tests errored'
   return 'Strider tests ' + (job.test_exitcode === 0 ? 'succeeded' : 'failed')
+}
+
+var
+    STRIDER_DATA_FOLDER = process.env.STRIDER_DATA_FOLDER || '/home/strider/.strider/data'
+  , UNIT_TEST_RUNNER_LOG = process.env.UNIT_TEST_RUNNER_LOG || 'unit-test-runner.log'
+  , UNIT_TEST_ERRORS_LOG = process.env.UNIT_TEST_ERRORS_LOG || 'unit-test-errors.log'
+;
+
+function readLog(job) {
+    var slug = job.project.name.replace('/', '-');
+    var projectFolder = [slug, job._id].join('-');
+    var BASE_PATH = path.resolve([ STRIDER_DATA_FOLDER, projectFolder ].join('/'));
+
+    var runnerLogPath = path.resolve([BASE_PATH, UNIT_TEST_RUNNER_LOG]);
+    var errorsLogPath = path.resolve([BASE_PATH, UNIT_TEST_ERRORS_LOG]);
+
+    if (job.errored || job.test_exitcode !== 0)
+        return fs.readFileSync(errorsLogPath).toString();
+
+    return fs.readFileSync(runnerLogPath).toString();
 }
 
 module.exports = {
@@ -30,10 +51,12 @@ module.exports = {
         var url = context.config.server_name + '/' + projectName + '/job/' + jobId
           , status = jobStatus(job)
           , description = jobDescription(job)
-        setStatus(token, url, data, status, description)
+          , report = readLog(job)
+
+        setStatus(token, url, data, status, description);
+        createComment(token, url, data, status, report);
       }
       io.on('job.doneAndSaved', onDoneAndSaved)
     })
   }
 }
-
